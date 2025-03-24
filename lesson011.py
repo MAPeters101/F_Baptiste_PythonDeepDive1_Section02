6adb198e41a7f950b80f54765f4ada51c5b00300
Mark Peters <mapeters101@earthlink.net>
2025-03-24 15:15:29 -0400

    Complete work on lesson011 from 12:10.

--- lesson011.py
+++ lesson011.py
@@ -51,6 +51,22 @@ if not found:
 print(l)
 print('-'*80)
 
+l = [1, 2, 3]
+val = 10
+idx = 0
+while idx < len(l):
+    if l[idx] == val:
+        break
+    idx += 1
+else:
+    l.append(val)
+print(l)
+
+
+
+
+print('-'*80)
+
 
 
 
