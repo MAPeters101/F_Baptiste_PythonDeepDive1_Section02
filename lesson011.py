ac88399c284dc896cdce7ec3cb59672cb1136cce
Mark Peters <mapeters101@earthlink.net>
2025-03-24 14:59:48 -0400

    Work on lesson011 from 6:45 to 7:45.

--- lesson011.py
+++ lesson011.py
@@ -12,12 +12,24 @@ while True:
         print('something')
 print('-'*80)
 
+# min_length = 2
+# name = input("Please enter your name: ")
+# while not(len(name) >= min_length and name.isprintable() and name.isalpha()):
+#     name = input("Please enter your name: ")
+#
+# print("Hello, {0}".format(name))
+# print('-'*80)
+
 min_length = 2
-name = input("Please enter your name: ")
-while not(len(name) >= min_length and name.isprintable() and name.isalpha()):
+while True:
     name = input("Please enter your name: ")
+    if len(name) >= min_length and name.isprintable() and name.isalpha():
+        break
 
 print("Hello, {0}".format(name))
 print('-'*80)
+
+
+
 
 
