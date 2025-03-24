472c83e621b58da4c6b00a027e30edb9af92cc62
Mark Peters <mapeters101@earthlink.net>
2025-03-24 00:22:06 -0400

    Work on lesson009 from 0:55 to 2:30.

--- lesson009.py
+++ lesson009.py
@@ -11,4 +11,44 @@ if a < 5:
     print('a < 5')
 else:
     print('a >= 5')
+print('-'*80)
+
+a = 10
+if a < 5:
+    print('a < 5')
+else:
+    if a < 10:
+        print('5 <= a < 10')
+    else:
+        print('a >= 10')
+
+a = 8
+if a < 5:
+    print('a < 5')
+else:
+    if a < 10:
+        print('5 <= a < 10')
+    else:
+        print('a >= 10')
+
+a = 4
+if a < 5:
+    print('a < 5')
+else:
+    if a < 10:
+        print('5 <= a < 10')
+    else:
+        print('a >= 10')
+
+a = 5
+if a < 5:
+    print('a < 5')
+else:
+    if a < 10:
+        print('5 <= a < 10')
+    else:
+        print('a >= 10')
+print('-'*80)
+
+
 
