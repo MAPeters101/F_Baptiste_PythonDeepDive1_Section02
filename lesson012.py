5324eac72166e0817ed468fa2ba6ede6ab94bdf7
Mark Peters <mapeters101@earthlink.net>
2025-03-24 16:15:44 -0400

    Work on lesson012 from 8:00 to 8:30.

--- lesson012.py
+++ lesson012.py
@@ -24,4 +24,20 @@ while a < 4:
     print('{0}, {1} - main loop'.format(a, b))
 print('-'*80)
 
+a = 0
+b = 2
+while a < 4:
+    print('-'*20)
+    a += 1
+    b -= 1
+    try:
+        a/b
+    except ZeroDivisionError:
+        print('{0}, {1} - division by 0'.format(a, b))
+        break
+    finally:
+        print('{0}, {1} - this always executes'.format(a, b))
+    print('{0}, {1} - main loop'.format(a, b))
+print('-'*80)
+
 
