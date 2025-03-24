b66813a95021fed61341145ff0d70f2d418f29ad
Mark Peters <mapeters101@earthlink.net>
2025-03-23 23:35:16 -0400

    Work on lesson007 to 14:00

---
+++ lesson007.py
@@ -0,0 +1,23 @@
+a = [1, 2, 3]
+a = [1, 2,
+     3, 4, 5]
+print(a)
+#a = [1 #item,
+#    2 ]
+a = [1, #item
+    2]
+a = [1 #item
+    ,2]
+
+a = (1 #comment
+    ,2 #comment
+    ,3)
+print(a)
+# a = {'key1': 1 #value for key 1
+#     ,'key2': 2 #value for key2 }
+# print(a)
+a = {'key1': 1 #value for key 1
+    ,'key2': 2 #value for key2
+    }
+print(a)
+print('-'*80)
