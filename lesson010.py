0ef6733f0b0e1f00e3879795181722f7ea54e3af
Mark Peters <mapeters101@earthlink.net>
2025-03-24 00:42:02 -0400

    Work on lesson010 from 2:30 to 5:00.

--- lesson010.py
+++ lesson010.py
@@ -14,6 +14,16 @@ def func_1():
 
 print(func_1)
 func_1()
+print('-'*80)
+
+def func_2(a: int, b: int):
+    return a*b
+print(func_2(2, 3))
+print(func_2('a', 3))
+print(func_2([1, 2], 3))
+#print(func_2('a', 'b'))
+print('-'*80)
+
 
 
 
