df9544253ef5a328b3ea3ea6827b7ed7bd87640b
Mark Peters <mapeters101@earthlink.net>
2025-03-24 16:13:18 -0400

    Work on lesson012 from 1:45 to 8:00.

--- lesson012.py
+++ lesson012.py
@@ -8,4 +8,20 @@ finally:
     print('this always executes')
 print('-'*80)
 
+a = 0
+b = 2
+while a < 4:
+    print('-'*20)
+    a += 1
+    b -= 1
+    try:
+        a/b
+    except ZeroDivisionError:
+        print('{0}, {1} - division by 0'.format(a, b))
+        continue
+    finally:
+        print('{0}, {1} - this always executes'.format(a, b))
+    print('{0}, {1} - main loop'.format(a, b))
+print('-'*80)
+
 
