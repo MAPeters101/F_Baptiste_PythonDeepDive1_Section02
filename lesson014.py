5965b6f0f9e893716283d6cd0c5d2b6d86370c8f
Mark Peters <mapeters101@earthlink.net>
2025-03-24 19:38:25 -0400

    Work on lesson014 from 3:20 to 6:20.

--- lesson014.py
+++ lesson014.py
@@ -9,7 +9,23 @@ r1 = Rectangle(10, 20)
 print(r1.width)
 r1.width = 100
 print(r1.width)
+print('-'*80)
 
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.height + self.width)
+
+r1 = Rectangle(10, 20)
+print(r1.area())
+print(r1.perimeter())
+print('-'*80)
 
 
 
