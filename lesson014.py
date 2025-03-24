1c173d91d112f8b7e31693b0831c5b9c51400fa1
Mark Peters <mapeters101@earthlink.net>
2025-03-24 19:34:08 -0400

    Work on lesson014 to 3:20.

---
+++ lesson014.py
@@ -0,0 +1,19 @@
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+r1 = Rectangle(10, 20)
+print(r1.width)
+r1.width = 100
+print(r1.width)
+
+
+
+
+
+
+
+
