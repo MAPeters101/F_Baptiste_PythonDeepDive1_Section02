4e41cedfc571bdf137338ff5507e435f203729d5
Mark Peters <mapeters101@earthlink.net>
2025-03-24 00:27:32 -0400

    Work on lesson009 from 2:30 to 4:30.

--- lesson009.py
+++ lesson009.py
@@ -50,5 +50,20 @@ else:
         print('a >= 10')
 print('-'*80)
 
+a = 25
+if a < 5:
+    print('a < 5')
+elif a < 10:
+    print('5 <= a < 10')
+elif a < 15:
+    print('10 <= a < 15')
+elif a < 20:
+    print('15 <= a < 20')
+else:
+    print('a > 20')
+print('-'*80)
+
+
+
 
 
