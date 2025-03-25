7789f500dafd122ec4a542264ab15505d219ce60
Mark Peters <mapeters101@earthlink.net>
2025-03-25 14:53:53 -0400

    Complete work on lesson014 from 37:20.

--- lesson014.py
+++ lesson014.py
@@ -282,9 +282,115 @@ print(r1)
 #r1.width = -100
 r1.width = 100
 print(r1)
-print('-'*80)
+print('='*80)
 
 
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        #print('getting width')
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width <= 0:
+            raise ValueError('Width must be positive.')
+        else:
+            self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        if height <= 0:
+            raise ValueError('Width must be positive.')
+        else:
+            self._height = height
+
+    def __str__(self):
+        return 'Rectangle: width={0}, height={1}'.format(self._width, self._height)
+
+    def __repr__(self):
+        return 'Rectangle({0}, {1})'.format(self._width, self._height)
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self._width == other._width and self._height == other._height
+        else:
+            return False
+
+r1 = Rectangle(10, 20)
+print(r1.width)
+print(r1)
+#r1.width = -100
+r1.width = 100
+print(r1)
+print()
+
+r1 = Rectangle(-100, 20)
+print(r1.width)
+print(r1)
+print()
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        #print('getting width')
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width <= 0:
+            raise ValueError('Width must be positive.')
+        else:
+            self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        if height <= 0:
+            raise ValueError('Width must be positive.')
+        else:
+            self._height = height
+
+    def __str__(self):
+        return 'Rectangle: width={0}, height={1}'.format(self._width, self._height)
+
+    def __repr__(self):
+        return 'Rectangle({0}, {1})'.format(self._width, self._height)
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self._width == other._width and self._height == other._height
+        else:
+            return False
+
+r1 = Rectangle(10, 20)
+print(r1.width)
+print(r1)
+#r1.width = -100
+r1.width = 100
+print(r1)
+print()
+
+# r1 = Rectangle(-100, 20)
+# print(r1.width)
+# print(r1)
+print()
 
 
 
