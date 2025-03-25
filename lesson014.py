1871d4401599b3ceb27a806a7fcb88048630587e
Mark Peters <mapeters101@earthlink.net>
2025-03-25 13:50:40 -0400

    Work on lesson014 from 17:40 to 21:00.

--- lesson014.py
+++ lesson014.py
@@ -158,13 +158,43 @@ r1 = Rectangle(10, 20)
 r2 = Rectangle(10, 20)
 print(r1 == r2)
 print(r1 == 100)
+print('-'*80)
 
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def area(self):
+        return self.width * self.height
 
+    def perimeter(self):
+        return 2 * (self.height + self.width)
 
+    def __str__(self):
+        return 'Rectangle: width={0}, height={1}'.format(self.width, self.height)
 
+    def __repr__(self):
+        return 'Rectangle({0}, {1})'.format(self.width, self.height)
 
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.width == other.width and self.height == other.height
+        else:
+            return False
 
+    def __lt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area() < other.area()
+        else:
+            return NotImplemented
+
+r1 = Rectangle(10, 20)
+r2 = Rectangle(100, 200)
+print(r1 < r2)
+print(r2 < r1)
+print(r2 > r1)
+#print(r1 <= r2)
 print('-'*80)
 
 
