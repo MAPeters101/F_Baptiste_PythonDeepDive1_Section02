4990d223ea83c84adef041e53e3f2cff99fe2988
Mark Peters <mapeters101@earthlink.net>
2025-03-25 14:18:07 -0400

    Work on lesson014 from 21:00 to 29:20 .

--- lesson014.py
+++ lesson014.py
@@ -197,6 +197,54 @@ print(r2 > r1)
 #print(r1 <= r2)
 print('-'*80)
 
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    def get_width(self):
+        return self._width
+
+    def set_width(self, width):
+        if width <= 0:
+            raise ValueError('Width must be positive.')
+        else:
+            self._width = width
+
+    def __str__(self):
+        return 'Rectangle: width={0}, height={1}'.format(self._width, self._height)
+
+    def __repr__(self):
+        return 'Rectangle({0}, {1})'.format(self._width, self._height)
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self._width == other._width and self._height == other._height
+        else:
+            return False
+
+r1 = Rectangle(10, 20)
+#print(r1.width)
+r1.width = -100
+print(r1.width)
+print(r1._width)
+print(r1.get_width())
+print(r1)
+
+#r1.set_width(-10)
+r1.set_width(100)
+print(r1)
+
+
+
+
+
+
+
+
+
+print('-'*80)
+
 
 
 
