7f6355141526762559bc20ad65bd808d58a71b5e
Mark Peters <mapeters101@earthlink.net>
2025-03-25 13:28:17 -0400

    Work on lesson014 from 6:20 to 9:10.

--- lesson014.py
+++ lesson014.py
@@ -27,8 +27,30 @@ print(r1.area())
 print(r1.perimeter())
 print('-'*80)
 
+print(str(r1))
+print(hex(id(r1)))
+print()
 
 
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.height + self.width)
+
+    def to_string(self):
+        return 'Rectangle: width={0}, height={1}'.format(self.width, self.height)
+
+r1 = Rectangle(10, 20)
+print(str(r1))
+print(hex(id(r1)))
+print(r1.to_string())
+print('-'*80)
 
 
 
